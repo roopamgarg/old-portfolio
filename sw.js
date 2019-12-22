3dbe5a549dbd2e8d1ea28f384e38343e411cf844
roopamgarg <roopamg777@gmail.com>
2019-12-22 13:11:58 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-7e3515a1c9f1b3264882.js"
+    "url": "webpack-runtime-aabb14bcdfbb3267437c.js"
   },
   {
-    "url": "commons-9c6655ba36ef3d604f44.js"
+    "url": "commons-4d71282ea766b042fec6.js"
   },
   {
     "url": "app-6a52d5e0a80c5b007cdf.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9f5a63d72d61aa69ccccb5e63e377693"
+    "revision": "8f654e8482b35fca8ce55706aa006513"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
